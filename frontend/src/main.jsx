1d60346009ec49fdf790b078b555624e64633f8b
happy <gillharpreetsingh211@gmail.com>
2025-02-26 09:08:36 +0530

    chakra config

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import { Provider } from "./components/ui/provider"  // ✅ Should resolve now
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </StrictMode>,
 )
